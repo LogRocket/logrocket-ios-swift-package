c606940b78679ccbbaa1540bd0d4fdfbbb25f2e9
James Logsdon <dwarf@girsbrain.org>
2022-09-23 14:00:55 -0400

    Add workflow to update package version

---
+++ .github/scripts/version-update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+declare RELEASE_VERSION
+RELEASE_VERSION="$(curl https://storage.googleapis.com/logrocket-ios/VERSION)"
+
+if [ -z "$RELEASE_VERSION" ]; then
+    echo "An issue was encountered fetching the current version!"
+    exit 1
+fi
+
+echo "Current SDK version: $RELEASE_VERSION"
+if git tag -l | grep "$RELEASE_VERSION"; then
+    echo "Swift Package version is already tagged for $RELEASE_VERSION."
+    exit 0
+fi
+
+echo "Updating Swift Package to $RELEASE_VERSION..."
+
+curl -sS "https://storage.googleapis.com/logrocket-ios/$RELEASE_VERSION/Package.swift" > "$GITHUB_WORKSPACE/Package.swift"
+
+git config user.email "action@github.com"
+git config user.name "GitHub Actions"
+git commit -am "Update to $RELEASE_VERSION"
+git tag "$RELEASE_VERSION"
+git push --tags origin main
