78aab57c41ca528adcd77e5d287201cdee484266
James Logsdon <dwarf@girsbrain.org>
2023-04-10 10:49:37 -0400

    Fix 1.13.0 Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,22 @@
-<?xml version='1.0' encoding='UTF-8'?><Error><Code>NoSuchKey</Code><Message>The specified key does not exist.</Message><Details>No such object: logrocket-ios/1.13.0/Package.swift</Details></Error>
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "LogRocket",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "LogRocket",
+            targets: ["LogRocket"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LogRocket",
+            url: "https://storage.googleapis.com/logrocket-ios/1.13.0/LogRocket.xcframework.zip",
+            checksum: "d1a1a816adb30b2fc409d8ff5c4619d7cfcfeecd67536652f2f98f06fc891028"
+        ),
+    ]
+)
