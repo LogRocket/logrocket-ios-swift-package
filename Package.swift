a9458751202566529ec09b3421315ffc9c45dc46
Deniss Kaibagarovs <deniss.kaibagarovs@gmail.com>
2023-10-31 17:17:49 +0000

    Fix checksum of 1.26.1 release
    The checksum was incorrect. That prevented SPM in my project from compiling successfully.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "LogRocket",
             url: "https://storage.googleapis.com/logrocket-ios/1.26.1/LogRocket.xcframework.zip",
-            checksum: "930c2b95a8286673414928907a8a04101e412ff482e7047f5df9eb03d7585e48"
+            checksum: "8655bef99b38c2d947f26b3c7a2233626157096332e9ba928a4a062738187f9c"
         ),
     ]
 )
