ba26955a17bbab8152eb72363e50e6b29f1e943f
LogRocket Deployer <support@logrocket.com>
2025-11-25 17:24:29 +0000

    Update to 1.59.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LogRocket",
-            url: "https://storage.googleapis.com/logrocket-ios/1.58.1/LogRocket.xcframework.zip",
-            checksum: "deb3ef5536316cb9f5d760814738ff22cb900512845ff93138241a4be0b80e24"
+            url: "https://storage.googleapis.com/logrocket-ios/1.59.0/LogRocket.xcframework.zip",
+            checksum: "d1872add4aecd7283bd34b646b7ed2575a29b0aa047dabeea65d642e69b7ae31"
         ),
     ]
 )
